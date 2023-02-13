a69a0c5e788c33c97253392074710026adfcef3e
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-13 20:32:42 +0300

    Решена задача по переворачиванию массива

---
+++ Example_001/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+
+    return result;
+
+} 
+
+void ReverseArray1(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length / 2; i++ )
+    {
+        int temp = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = temp;
+    }
+}
+
+int[] ReverseArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+
+    return result;
+}
+
+Console.Clear();
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+ReverseArray1(array);
+int[] reversed = ReverseArray2(array);
+Console.WriteLine(String.Join(", ", array));
