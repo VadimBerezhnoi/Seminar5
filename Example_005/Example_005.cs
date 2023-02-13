45124a08ae8b58eae154019c75eb19a4dad89dd5
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-13 21:32:22 +0300

    Решена задача с копированием существующего массива

---
+++ Example_005/Example_005.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int[] CopyArray(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+int[] array = { 1, 2, 3, 55 };
+
+int[] newArray = CopyArray(array);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine();
+Console.WriteLine(String.Join(", ", newArray));
