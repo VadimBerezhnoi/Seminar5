2fcaa4daf93fa0d80c0c7b89c9c20486f2eee912
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-13 21:23:06 +0300

    Решена задача с последовательностью Фибоначии

---
+++ Example_004/Example_004.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int[] arrayFib(int n)
+{
+    int[] array = new int[n];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+
+    return array;
+}
+
+Console.Write("Введите число ");
+int nFib = int.Parse(Console.ReadLine()!);
+Console.Write(string.Join(", ", arrayFib(nFib)));
