a85cd9a1c781545dfaab66c5541191bb8f3535ea
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-13 21:01:28 +0300

    Решена задача по переводу числа из десятичной системы счисления в двоичную

---
+++ Example_003/Example_003.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+string ConvertDecToBin(int number)
+{
+    string result = String.Empty;
+    while (number != 0)
+    {
+        result = (number % 2).ToString() + result;
+        number /= 2;
+    }
+    return result;
+}
+
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ConvertDecToBin(n));
+
