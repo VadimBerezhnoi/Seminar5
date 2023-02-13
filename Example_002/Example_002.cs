f48e64fcc53f79f8ec5772865d1de35b3a9a11ae
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-13 20:46:09 +0300

    Решена задача с треугольником

---
+++ Example_002/Example_002.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+bool FindTriangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+Console.WriteLine("Введите сторону A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите сторону B: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите сторону C: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (FindTriangle(a, b, c))
+{
+    System.Console.WriteLine("Треугольник существует");
+}
+
+else
+{
+    System.Console.WriteLine("Треугольник не существует");
+}
